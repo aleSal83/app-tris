c2591bc2748d6015eb9b91eab866cb12da61aac3
alessio salierno <alessio.salierno@msc.com>
2023-04-03 16:42:50 +0200

    leave comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-// Angular modules
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-// Application imports
+
 import { AppRoutingModule } from './app-routing.module';
-//import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
-// Nebular modules
-//import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 
-// Angular/material modules
 import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,7 +17,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatRadioModule } from '@angular/material/radio';
 
-// Service worker module
+
 import { SquareComponent } from './component/square/square.component';
 import { GameViewComponent } from './component/game-view/game-view.component';
 import { TopViewComponent } from './component/top-view/top-view.component';
